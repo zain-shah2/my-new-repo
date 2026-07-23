f6b283c71597b9fa950e00fd6e899145389f342d
zain-shah2 <zainshah88900@gmail.com>
2026-07-23 16:56:40 +0500

    dairy.py

---
+++ dairy.py
@@ -0,0 +1,47 @@
+# age =  16
+
+# if(age>=18):
+#     print("can marrige")
+#     print("can drive")
+#     print("can live")
+# elif(age<18):
+#     print("can't marriage")
+#     print("can't drive")
+# elif(age>50):
+#     print("can't marriage")
+#     print("can't drive")
+
+# marks = int(input("enter the marks of student :"))
+
+# if(marks >=90):
+#     print("grade A")
+# elif(marks >=80 and marks <90):
+#     print("grade B")
+# elif(marks >=70 and marks <80):
+#     print("grade C")
+# elif(marks >=60 and marks <70):
+#     print("grade D")
+# else:
+#     print("you are fail")
+
+
+# num = int(input("enter your digit :"))
+# rem = num % 2
+# if(rem == 0) :
+#     print("EVEN")
+# else :
+#     print("ODD")
+
+
+a = int(input("enter your digit :"))
+b= int(input("enter your digit :"))
+c = int(input("enter your digit :"))
+
+if( a>=b and a>=c):
+    print("greatest no is",a)
+elif(b>=c):
+    print("greatest no is b")
+else:
+    print("greatest no is ", c)
+
+
